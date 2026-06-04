472fc26129686515c11156ffab7801876863269a
Arnav8452 <arnavchandra911@gmail.com>
2026-06-04 20:00:26 +0530

    chore: stop tracking sensitive leads data, temporary test scripts, and log files

--- scripts/test_puppeteer.ts
+++
@@ -1,9 +0,0 @@
-import puppeteer from 'puppeteer';
-async function test() {
-    console.log("Launching...");
-    const browser = await puppeteer.launch({ headless: false });
-    console.log("Launched!");
-    await browser.close();
-    console.log("Closed!");
-}
-test();
